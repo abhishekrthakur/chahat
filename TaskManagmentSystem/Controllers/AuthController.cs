ed0eadb22dbf76efcec9e2162cff4d64c9584cc3
Vivek Rana <ranavivek367@gmail.com>
2024-07-12 00:14:11 +0530

    Auth - login and register  UI

---
+++ TaskManagmentSystem/Controllers/AuthController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using TaskManagmentSystem.Models;
+
+namespace TaskManagmentSystem.Controllers
+{
+    public class AuthController : Controller
+    {
+        public IActionResult Login()
+        {
+            return View("~/Views/AuthView/Login.cshtml");
+        }
+
+        public IActionResult Register()
+        {
+            return View("~/Views/AuthView/Register.cshtml");
+        }
+
+        public IActionResult AuthenticateUser(string userName, string password)
+        {
+            // add auth repo
+            return View("~/Views/AuthView/Login.cshtml");
+        }
+
+        public IActionResult RegisterUser(User user)
+        {
+            // add register repo
+            return View("~/Views/AuthView/Login.cshtml");
+        }
+    }
+}

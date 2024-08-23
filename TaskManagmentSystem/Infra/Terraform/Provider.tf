518267cdde148a618037f706929bda537e358cbf
Vivek Rana <ranavivek367@gmail.com>
2024-08-24 00:29:10 +0530

    adding backend state managment

--- TaskManagmentSystem/Infra/Terraform/Provider.tf
+++ TaskManagmentSystem/Infra/Terraform/Provider.tf
@@ -5,6 +5,13 @@ terraform {
       version = "=3.116.0"
     }
   }
+
+  backend "azurerm" {
+    resource_group_name   = "Needed-Resource"
+    storage_account_name  = "requiredresources"
+    container_name        = "terraform-state"
+    key                   = "terraform.tfstate"
+  }
 }
 
 provider "azurerm" {
